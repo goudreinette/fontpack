cd912a25f7584988a458be02f729ff64cc364403
Rein van der Woerd <reinvanderwoerd@me.com>
2018-12-12 12:16:44 +0100

    Proton Native

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+import React, { Component } from 'react'; // import from react
+import {render, Window, App, FontButton, Box, Button, Text} from 'proton-native'
 const fs = require('fs');
 const homedir = require('os').homedir();
-const {exec} = require('child_process')
 const path = require('path');
 const Datauri = require('datauri');
 const datauri = new Datauri();
 const fontManager = require('font-manager')
-const libui = require('libui-node')
 
 
 function makeCss({family, weight, italic}, data) {
-	return `
-	
+    return `
 @font-face {
 	font-family: '${family}';
 	src: url('${data}');
@@ -22,72 +21,65 @@ function makeCss({family, weight, italic}, data) {
 
 
 function familyToCss(familyName = 'Helvetica') {
-	let nonItalic = fontManager.findFontsSync({family: familyName, italic: false})
-	let italic = fontManager.findFontsSync({family: familyName, italic: true})
-	let fonts = [...nonItalic, ...italic]
-	
-	let fontfaceRules = fonts.map(variant => {
-		let fileContent = fs.readFileSync(variant.path)
-		let ext = path.extname(variant.path)
-		let fontData = datauri.format(ext, fileContent).content
-		return makeCss(variant, fontData)
-	})
-	
-	return fontfaceRules.join()
+    let nonItalic = fontManager.findFontsSync({family: familyName, italic: false})
+    let italic = fontManager.findFontsSync({family: familyName, italic: true})
+    let fonts = [...nonItalic, ...italic]
+
+    let fontfaceRules = fonts.map(variant => {
+        let fileContent = fs.readFileSync(variant.path)
+        let ext = path.extname(variant.path)
+        let fontData = datauri.format(ext, fileContent).content
+        return makeCss(variant, fontData)
+    })
+
+    return fontfaceRules.join()
 }
 
 
 function writeCssFileFor(family) {
-	let path = `${homedir}/Desktop/${family}.css`
-	fs.writeFileSync(path, familyToCss(family))
-	return path
+    let path = `${homedir}/Desktop/${family}.css`
+    fs.writeFileSync(path, familyToCss(family))
+    return path
 }
 
 
 
 
-
-function main() {	
-	// font chooser
-	var fontbutton = new libui.UiFontButton()
-	fontbutton.onChanged(() => {
-		var family = fontbutton.font.getFamily()
-		console.log(`Selected: ${family}`);
-	});
-	
-		
-	// label
-	var label = new libui.UiLabel()
+class Example extends Component {
+    state = {
+        font: {fontFamily: 'Helvetica'},
+        statusMsg: ''
+    }
 
 
-	// button -----
-	var button = new libui.UiButton();
-	button.text = "Generate CSS file"
-	button.onClicked(() => {
-		let family = fontbutton.font.getFamily()
-		let path = writeCssFileFor(family)
-		label.text = `Generated ${path}`
-		//exec(`open ${path}`)
-	})
-	
-	
-	// add children to window  ----
-	var win = new libui.UiWindow("Font Packager", 320, 80, false);
-	win.margined = true;
-	var box = new libui.UiVerticalBox()
-	win.setChild(box)
-	box.append(fontbutton, false)
-	box.append(button, false)
-	box.append(label, false)
-	
-	
-	// loop ----
-	win.onClosing(() => {
-		win.close();
-		libui.stopLoop();
-	});
-	libui.startLoop();
-	win.show();
+    generate() {
+        console.log(this.state.font)
+        let family = this.state.font.fontFamily
+        let path = writeCssFileFor(family)
+        this.setState({statusMsg: `Generated ${path}`})
+    }
+
+    render() { // all Components must have a render method
+    return (
+      <App> // you must always include App around everything
+        <Window title="FontPack" size={{w: 300, h: 50}} menuBar={false}>
+            <Box padded={false}>
+                <FontButton
+                    stretchy={false}
+                    onChange={v => this.setState({ font: v })}
+                />
+                <Button
+                    onClick={_ => this.generate()}
+                    style={{ height: '100px' }}
+                >
+                    Generate CSS file
+                </Button>
+                <Text >{this.state.statusMsg}</Text>
+            </Box>
+        </Window>
+      </App>
+    );
+  }
 }
 
-main()
+render(<Example />); // and finally render your main component
